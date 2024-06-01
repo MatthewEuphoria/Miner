197edf17a0c725f1651f01447e11e2e65099dab9
EA <czarnymatt@gmail.com>
2024-06-01 10:03:50 -0500

    Add files via upload

--- miner.js
+++ miner.js
@@ -1,57 +1,602 @@
-const { ethers } = require("ethers");
-const fs = require("fs");
-const path = require("path");
-
-// Load the ABI from the contract file
-const abiPath = path.join(__dirname, "MineableERC20.json");
-const abi = JSON.parse(fs.readFileSync(abiPath));
+console.log("miner.js script loaded");
 
 // Replace with your contract address
 const contractAddress = "0xEa62886E5580d005fe9cd5db1e41D93Dc0771300";
 
-// Replace with your provider (e.g., Infura, Alchemy, or local node)
-const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID");
 
-// Replace with your wallet private key
-const privateKey = "YOUR_PRIVATE_KEY";
-const wallet = new ethers.Wallet(privateKey, provider);
-
-// Initialize the contract
-const contract = new ethers.Contract(contractAddress, abi, wallet);
-
-async function mine() {
-    const currentChallenge = await contract.currentChallenge();
-    const difficulty = await contract.difficulty();
-    const address = wallet.address;
-
-    console.log(`Current Challenge: ${currentChallenge}`);
-    console.log(`Difficulty: ${difficulty.toString()}`);
-    console.log(`Mining with address: ${address}`);
-
-    let nonce = 0;
-    while (true) {
-        const hash = ethers.utils.keccak256(
-            ethers.utils.defaultAbiCoder.encode(
-                ["bytes32", "address", "uint256"],
-                [currentChallenge, address, nonce]
-            )
-        );
-        if (BigInt(hash) < BigInt(difficulty)) {
-            console.log(`Found valid nonce: ${nonce}`);
-            console.log(`Hash: ${hash}`);
-            try {
-                const tx = await contract.mine(nonce);
-                console.log("Transaction sent:", tx.hash);
-                await tx.wait();
-                console.log("Transaction confirmed");
-                break;
-            } catch (error) {
-                console.error("Error submitting transaction:", error);
-                break;
+// Load the ABI from the contract file
+const abi = [
+    {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "allowance",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "needed",
+                "type": "uint256"
             }
-        }
-        nonce++;
+        ],
+        "name": "ERC20InsufficientAllowance",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "sender",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "balance",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint256",
+                "name": "needed",
+                "type": "uint256"
+            }
+        ],
+        "name": "ERC20InsufficientBalance",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "approver",
+                "type": "address"
+            }
+        ],
+        "name": "ERC20InvalidApprover",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "receiver",
+                "type": "address"
+            }
+        ],
+        "name": "ERC20InvalidReceiver",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "sender",
+                "type": "address"
+            }
+        ],
+        "name": "ERC20InvalidSender",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            }
+        ],
+        "name": "ERC20InvalidSpender",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnableInvalidOwner",
+        "type": "error"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "OwnableUnauthorizedAccount",
+        "type": "error"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Approval",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "miner",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "reward",
+                "type": "uint256"
+            }
+        ],
+        "name": "Mined",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "previousOwner",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "OwnershipTransferred",
+        "type": "event"
+    },
+    {
+        "anonymous": false,
+        "inputs": [
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "indexed": true,
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "indexed": false,
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "Transfer",
+        "type": "event"
+    },
+    {
+        "inputs": [],
+        "name": "INITIAL_SUPPLY",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "owner",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            }
+        ],
+        "name": "allowance",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "spender",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "approve",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "account",
+                "type": "address"
+            }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "challengeCounter",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "currentChallenge",
+        "outputs": [
+            {
+                "internalType": "bytes32",
+                "name": "",
+                "type": "bytes32"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+            {
+                "internalType": "uint8",
+                "name": "",
+                "type": "uint8"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "difficulty",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "nonce",
+                "type": "uint256"
+            }
+        ],
+        "name": "mine",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "owner",
+        "outputs": [
+            {
+                "internalType": "address",
+                "name": "",
+                "type": "address"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "renounceOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "reward",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "_difficulty",
+                "type": "uint256"
+            }
+        ],
+        "name": "setDifficulty",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+            {
+                "internalType": "string",
+                "name": "",
+                "type": "string"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "tokensMinted",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transfer",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "from",
+                "type": "address"
+            },
+            {
+                "internalType": "address",
+                "name": "to",
+                "type": "address"
+            },
+            {
+                "internalType": "uint256",
+                "name": "value",
+                "type": "uint256"
+            }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+            {
+                "internalType": "bool",
+                "name": "",
+                "type": "bool"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "inputs": [
+            {
+                "internalType": "address",
+                "name": "newOwner",
+                "type": "address"
+            }
+        ],
+        "name": "transferOwnership",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    }
+];
+
+// Initialize the provider
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+let wallet;
+let contract;
+window.isInitialized = false; // Make isInitialized a global variable
+
+async function init() {
+    console.log("Initializing...");
+
+    try {
+        // Wait for the MetaMask provider to be ready
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        wallet = signer;
+        console.log("Wallet connected: ", await wallet.getAddress());
+
+        contract = new ethers.Contract(contractAddress, abi, signer);
+        console.log("Contract initialized");
+
+        window.isInitialized = true; // Set isInitialized to true after initialization
+        console.log("Initialization complete");
+    } catch (error) {
+        console.error("Error during initialization:", error);
     }
 }
 
-mine().catch(console.error);
+async function mine() {
+    if (!window.isInitialized) {
+        console.error("Contract is not initialized.");
+        return;
+    }
+
+    console.log("Mining started...");
+    document.getElementById("status").innerText = "Mining started...";
+    try {
+        const currentChallenge = await contract.currentChallenge();
+        const difficulty = await contract.difficulty();
+        const address = await wallet.getAddress();
+
+        document.getElementById("challenge").innerText = `Current Challenge: ${currentChallenge}`;
+        document.getElementById("difficulty").innerText = `Difficulty: ${difficulty.toString()}`;
+        document.getElementById("address").innerText = `Mining with address: ${address}`;
+
+        let nonce = 0;
+        while (true) {
+            const hash = ethers.utils.keccak256(
+                ethers.utils.defaultAbiCoder.encode(
+                    ["bytes32", "address", "uint256"],
+                    [currentChallenge, address, nonce]
+                )
+            );
+            if (BigInt(hash) < BigInt(difficulty)) {
+                console.log(`Found valid nonce: ${nonce}`);
+                console.log(`Hash: ${hash}`);
+                document.getElementById("status").innerText = `Found valid nonce: ${nonce}`;
+                try {
+                    const tx = await contract.mine(nonce);
+                    console.log("Transaction sent:", tx.hash);
+                    document.getElementById("status").innerText = "Transaction sent. Waiting for confirmation...";
+                    await tx.wait();
+                    console.log("Transaction confirmed");
+                    document.getElementById("status").innerText = "Transaction confirmed. Mining complete.";
+                    break;
+                } catch (error) {
+                    console.error("Error submitting transaction:", error);
+                    document.getElementById("status").innerText = "Error submitting transaction.";
+                    break;
+                }
+            }
+            nonce++;
+        }
+    } catch (error) {
+        console.error("Error during mining:", error);
+        document.getElementById("status").innerText = "Error during mining.";
+    }
+}
+
+// Initialize the DApp
+init().catch(console.error);
+
+window.mine = mine; // Export the mine function to the global window object
